7a28ac5a4e5d967ea53292d009f488427cf38d7a
holykim <holykim@mail.ru>
2023-11-30 18:36:43 +0300

    added main+staff+router

---
+++ src/pages/main/styled-Main.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+import img from "../../assets/images/bgc.png"
+
+export const Img = styled.img`
+  color: white;
+`
+
+export const MainDiv = styled.div`
+  background: url(${img}) no-repeat center center;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+`
