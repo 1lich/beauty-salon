c0223d2d67c4f63fefe191079823e2e69f097cd0
Alister <holykim@mail.ru>
2023-12-17 20:34:57 +0300

    add price and effector

--- src/pages/price/Price.jsx
+++ src/pages/price/Price.jsx
@@ -1,6 +1,48 @@
+import { priceListStore } from "../../services/commonDomain"
+import { useUnit } from "effector-react"
+
 import Layout from "../../components/Layout"
+import {
+  DescDiv,
+  PriceDiv,
+  InnerPriceDiv,
+  OuterPriceDiv,
+  PriceGrid,
+  PriceH1,
+  PriceImg,
+  PriceValue,
+  TitleDiv,
+  CenterDiv,
+} from "./styled-Price"
+import img from "../../assets/images/wash.png"
+
 const Price = () => {
-  return <Layout title="Цены на услуги">ЦЕНЫ НА УСЛУГИ</Layout>
+  const priceList = useUnit(priceListStore)
+  console.log(priceList)
+
+  return (
+    <Layout title="Цены на услуги">
+      <PriceH1>Цены на услуги</PriceH1>
+      <CenterDiv>
+        <PriceGrid>
+          <PriceImg src={img}></PriceImg>
+          <PriceDiv>
+            {priceList.map(({ id, title, price, description } = priceList) => {
+              return (
+                <OuterPriceDiv key={id}>
+                  <InnerPriceDiv>
+                    <TitleDiv>{title}</TitleDiv>
+                    <PriceValue>{price}</PriceValue>
+                  </InnerPriceDiv>
+                  <DescDiv>{description}</DescDiv>
+                </OuterPriceDiv>
+              )
+            })}
+          </PriceDiv>
+        </PriceGrid>
+      </CenterDiv>
+    </Layout>
+  )
 }
 
 export default Price
