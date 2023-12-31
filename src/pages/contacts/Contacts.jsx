d8be2c5bb55c269c47eb55bb275cb3f08e155873
1lich <56218879+1lich@users.noreply.github.com>
2023-12-31 23:53:48 +0300

    Localmain (#11)
    * added main+staff+router
    
    * add router
    
    * add header
    
    * add header + icons
    
    * add footer and links
    
    * azaza
    
    * azaza2
    
    * remove insta in header
    
    * add staff and service
    
    * fix branch
    
    * fix header
    
    * fix services
    
    * add price and effector
    
    * add price and prices
    
    * add map

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,7 +1,15 @@
 import Layout from "../../components/Layout"
+import MapComponent from "./MapComponent"
+import MapComp from "./MapComp"
+import { StyledH1 } from "./styled-Contacts"
 
 const Contacts = () => {
-  return <Layout title="Контакты">CONTACTS</Layout>
+  return (
+    <Layout title="Контакты">
+      <StyledH1>Контакты</StyledH1>
+      <MapComp />
+    </Layout>
+  )
 }
 
 export default Contacts
