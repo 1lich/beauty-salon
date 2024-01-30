8b40bbb4112602e0b098aa098e92c0ea14573319
1lich <56218879+1lich@users.noreply.github.com>
2024-01-30 21:53:54 +0300

    Localmain (#16)
    * added main+staff+router
    
    * add router
    
    * add header
    
    * add header + icons
    
    * add footer and links
    
    * azaza
    
    * azaza2
    
    * remove insta in header
    
    * add staff and service
    
    * fix branch
    
    * fix header
    
    * fix services
    
    * add price and effector
    
    * add price and prices
    
    * add map
    
    * added map
    
    * added margin to staff
    
    * add db
    
    * add db
    
    * add db
    
    * add db
    
    * fix effector by importing init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import "./services/init"
 import "./App.css"
 import Main from "./pages/main/Main"
 import Staff from "./pages/staff/Staff"
--- src/services/init.js
+++ src/services/init.js
@@ -1,1 +1,3 @@
 import "./commonInit"
+import "./commonDomain"
+
