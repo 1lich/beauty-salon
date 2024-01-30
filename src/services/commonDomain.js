ea2c737774a0523a2499c637343809e83d9f0d64
1lich <56218879+1lich@users.noreply.github.com>
2024-01-30 14:49:21 +0300

    Localmain (#13)
    * added main+staff+router
    
    * add router
    
    * add header
    
    * add header + icons
    
    * add footer and links
    
    * azaza
    
    * azaza2
    
    * remove insta in header
    
    * add staff and service
    
    * fix branch
    
    * fix header
    
    * fix services
    
    * add price and effector
    
    * add price and prices
    
    * add map
    
    * added map
    
    * added margin to staff
    
    * add db

--- src/services/commonDomain.js
+++ src/services/commonDomain.js
@@ -1,30 +1,23 @@
 import { createDomain } from "effector"
-
+import axios from "axios"
 const commonDomain = createDomain("commonDomain")
 
-export const getPriceListEvent = commonDomain.createEvent("getPriceList")
+export const priceListStore = commonDomain.createStore([])
+export const getPriceListFx = commonDomain.createEffect(async () => {
+  const getPriceList = await axios
+    .get("http://192.168.31.131:5000/api/data")
+    .then((response) => {
+      console.log("received from express", response.data)
+      return response.data
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 
-export const priceListStore = commonDomain.createStore([
-  {
-    id: 0,
-    title: "Женская стрижка",
-    price: "7000",
-    description: "бреем налысо",
-  },
-  { id: 1, title: "Укладка", price: "5000", description: "клей момент" },
-  {
-    id: 2,
-    title: "Свадебная прическа",
-    description: "как у старика извращенца",
-  },
-  {
-    id: 3,
-    title: "Мытье головы",
-    price: "1000",
-    description: "слюна и легкий массаж",
-  },
-  { id: 4, title: "Подрочить", price: "300", description: "двумя руками" },
-])
+  return getPriceList
+})
 
-priceListStore.on(getPriceListEvent, (_, payload) => payload)
-//priceListStore.watch((data) => console.log(`data: ${data}`))
+priceListStore.on(getPriceListFx.done, (_, payload) => payload)
+priceListStore.watch((data) => console.log("data in store", data.result))
+getPriceListFx.doneData.watch((data) => console.log("received data:", data))
+getPriceListFx()
